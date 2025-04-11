c00ed7098ffff41bee9f16c47662fa5170416f16
thomsonnapitupulu <thomson.p.napitupulu@gmail.com>
2025-04-11 12:49:18 +0700

    return random greeting

--- hello/main.go
+++ hello/main.go
@@ -15,7 +15,7 @@ func main() {
 	log.SetFlags(0)
 
 	// Request a greeting message.
-	m, err := greetings.Hello("")
+	m, err := greetings.Hello("Gladys")
 	// If an error was returned, print it to the console and
 	// exit the program.
 	if err != nil {
