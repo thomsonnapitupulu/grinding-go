00d60c8735ab8922bcc577d44300f39e90acee34
thomsonnapitupulu <thomson.p.napitupulu@gmail.com>
2025-04-11 20:45:32 +0700

    nothing but messyness

--- basics/main.go
+++ basics/main.go
@@ -2,9 +2,31 @@ package main
 
 import (
 	"fmt"
-	"math"
+	"math/cmplx"
+)
+
+func add(x, y int) int {
+	return x + y
+}
+
+func swap(x, y string) (string, string) {
+	return y, x
+}
+
+func split(sum int) (x, y int) {
+	x = sum * 4 / 9
+	y = sum - x
+	return
+}
+
+var (
+	ToBe   bool       = false
+	MaxInt uint64     = 1<<64 - 1
+	z      complex128 = cmplx.Sqrt(-5 + 12i)
 )
 
 func main() {
-	fmt.Printf("Now you have %g problems.\n", math.Sqrt(7))
+	fmt.Printf("Type: %T Value: %v\n", ToBe, ToBe)
+	fmt.Printf("Type: %T Value: %v\n", MaxInt, MaxInt)
+	fmt.Printf("Type: %T Value: %v\n", z, z)
 }
